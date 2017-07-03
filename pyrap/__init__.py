dc71a2e5343317718c87a99985f4d24dae67cb7a
Daniel Nyga <nyga@cs.uni-bremen.de>
2017-07-03 16:06:17 +0200

    automatically include 3rdparty modules into sys.path

--- pyrap/__init__.py
+++ pyrap/__init__.py
@@ -1,3 +1,12 @@
+import os
+import sys
+
+import locations
+
+trdparty_modules = ['dnutils']
+for mod in trdparty_modules:
+    sys.path.append(os.path.join(locations.trdparty, mod))
+
 from base import register_app
 from base import run
 from base import session
