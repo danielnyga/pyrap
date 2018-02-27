86306ba61d8760add4f21f754d47b9ef8a27b58d
Mareike <mareikep@cs.uni-bremen.de>
2018-02-27 20:47:12 +0100

    fix loading js files

--- python2/pyrap/engine.py
+++ python2/pyrap/engine.py
@@ -535,10 +535,12 @@ class SessionRuntime(object):
                 files = self.mngr.config.requirecss
             for p in files:
                 if os.path.isfile(p):
-                    self.requirecss(p)
+                    with open(p) as fi:
+                        self.requirecss(fi)
                 elif os.path.isdir(p):
                     for f in [x for x in os.listdir(p) if x.endswith('.css')]:
-                        self.requirecss(f)
+                        with open(f) as fi:
+                            self.requirecss(fi)
         self.create_display()
 
     def ensurejsresources(self, jsfiles, name=None, force=False):
