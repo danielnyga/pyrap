bef4d366da691684087d5aba281967e2ae8f9dd4
Mareike <mareikep@cs.uni-bremen.de>
2019-03-29 12:38:29 +0100

    update init

--- _version/__init__.py
+++ _version/__init__.py
@@ -8,9 +8,9 @@ __all__ = [
     'VERSION_MAJOR',
     'VERSION_MINOR',
     'VERSION_PATCH',
-    'VERSION_STRING',
-    '__version__',
-    '__basedir__'
+    'VERSION_STRING_FULL',
+    'VERSION_STRING_SHORT',
+    '__version__'
 ]
 
 VERSION_MAJOR = 0
@@ -21,10 +21,3 @@ VERSION_STRING_FULL = '%s.%s.%s' % (VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH)
 VERSION_STRING_SHORT = '%s.%s' % (VERSION_MAJOR, VERSION_MINOR)
 
 __version__ = VERSION_STRING_FULL
-
-if sys.version_info[0] == 2:
-    __basedir__ = 'python2'
-elif sys.version_info[0] == 3:
-    __basedir__ = 'python3'
-else:
-    raise Exception('Unsupported Python version: %s' % sys.version_info[0])
