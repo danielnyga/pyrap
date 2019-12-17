37adf8ab1d0cfa8ea9e359c267dd9064edf7c0ce
Mareike Picklum <mareikep@cs.uni-bremen.de>
2019-12-17 16:32:22 +0100

    update template to inherit from d3widget

--- pyrap/pwt/template/template.js
+++ pyrap/pwt/template/template.js
@@ -97,7 +97,7 @@ pwt_template.Template.prototype = {
      * retrieves the svg as text to save it to a file
      */
     retrievesvg : function ( args ) {
-        rwt.remote.Connection.getInstance().getRemoteObject( this ).set( args.type, this._svg.node().outerHTML );
+        rwt.remote.Connection.getInstance().getRemoteObject( this ).set( args.type, [this._svg.node().outerHTML, args.fname] );
     },
 
     /**
--- pyrap/pwt/template/template.py
+++ pyrap/pwt/template/template.py
@@ -1,95 +1,26 @@
 import os
 
 from dnutils.tools import ifnone
-
 from pyrap import session, locations
-from pyrap.communication import RWTCreateOperation, RWTCallOperation, \
-    RWTSetOperation
-from pyrap.constants import d3wrapper
-from pyrap.events import OnSelect, _rwt_event, OnSet
-from pyrap.ptypes import BitField
-from pyrap.pwt.pwtutils import downloadsvg, downloadpdf
+from pyrap.events import OnSelect, OnSet
+from pyrap.pwt.d3widget.d3widget import D3Widget
 from pyrap.themes import WidgetTheme
-from pyrap.widgets import Widget, constructor
+from pyrap.widgets import constructor, Widget
 
 
-class Template(Widget):
+class Template(D3Widget):
 
     _rwt_class_name = 'pwt.customs.Template'
-    _defstyle_ = BitField(Widget._defstyle_)
 
     @constructor('Template')
     def __init__(self, parent, opts=None, **options):
-        Widget.__init__(self, parent, **options)
+        D3Widget.__init__(self, parent, os.path.join(locations.pwt_loc, 'template', 'template.css'), version=3, opts=opts, css=None, **options)
         self.theme = TemplateTheme(self, session.runtime.mngr.theme)
-        self._requiredjs = [os.path.join(locations.trdparty, 'd3', 'd3.v3.min.js')]
-        with open(os.path.join(locations.trdparty, 'd3', 'd3.v3.min.js'), 'r') as f:
-            cnt = d3wrapper.format(**{'d3content': f.read()})
-            session.runtime.ensurejsresources(cnt, name='d3.v3.min.js', force=True)
-        with open(os.path.join(locations.pwt_loc, 'template', 'template.css')) as fi:
-            session.runtime.requirecss(fi)
         self._data = {}
         self._opts = opts
         self.on_select = OnSelect(self)
         self.on_set = OnSet(self)
         self.svg = None
-
-    def _create_rwt_widget(self):
-        options = Widget._rwt_options(self)
-        if self._opts:
-            options.options = self._opts
-        session.runtime << RWTCreateOperation(self.id, self._rwt_class_name, options)
-
-    def compute_size(self):
-        w, h = Widget.compute_size(self.parent)
-
-        padding = self.theme.padding
-        if padding:
-            w += ifnone(padding.left, 0) + ifnone(padding.right, 0)
-            h += ifnone(padding.top, 0) + ifnone(padding.bottom, 0)
-        margin = self.theme.margin
-        if margin:
-            w += ifnone(margin.left, 0) + ifnone(margin.right, 0)
-            h += ifnone(margin.top, 0) + ifnone(margin.bottom, 0)
-        t, r, b, l = self.theme.borders
-        w += ifnone(l, 0, lambda b: b.width) + ifnone(r, 0, lambda b: b.width)
-        h += ifnone(t, 0, lambda b: b.width) + ifnone(b, 0, lambda b: b.width)
-        return w, h
-
-    def _handle_notify(self, op):
-        events = {'Selection': self.on_select}
-        if op.event not in events:
-            return Widget._handle_notify(self, op)
-        events[op.event].notify(_rwt_event(op))
-        return True
-
-    def _handle_set(self, op):
-        Widget._handle_set(self, op)
-        for key, value in op.args.items():
-            if key == 'svg':
-                downloadsvg(op.args['svg'], self.width.value, self.height.value, os.path.join(locations.pwt_loc, 'template', 'template.css'), name=__class__.__name__)
-            if key == 'pdf':
-                downloadpdf(op.args['pdf'], self.width.value, self.height.value, os.path.join(locations.pwt_loc, 'template', 'template.css'), name=__class__.__name__)
-        self.on_set.notify(_rwt_event(op))
-
-    def play(self):
-        session.runtime << RWTCallOperation(self.id, 'play', {})
-
-    @property
-    def data(self):
-        return self._data
-
-    def clear(self):
-        self._data = {}
-        self._opts = []
-        session.runtime << RWTCallOperation(self.id, 'clear', {})
-
-    def setdata(self, data):
-        self._data = data
-        session.runtime << RWTSetOperation(self.id, {'data': data})
-
-    def download(self, pdf=False):
-        session.runtime << RWTCallOperation(self.id, 'retrievesvg', {'type': 'pdf' if pdf else 'svg'})
 
 
 class TemplateTheme(WidgetTheme):
