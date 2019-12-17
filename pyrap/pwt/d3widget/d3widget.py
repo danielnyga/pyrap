cf04db95241fc39c9d34342551236c75d74badc5
Mareike Picklum <mareikep@cs.uni-bremen.de>
2019-12-17 16:35:16 +0100

    move unnecessary content to d3widget

--- pyrap/pwt/d3widget/d3widget.py
+++ pyrap/pwt/d3widget/d3widget.py
@@ -1,14 +1,42 @@
+import io
 import os
+import tempfile
 
 from pyrap import session, locations
 from pyrap.communication import RWTCreateOperation, RWTCallOperation, RWTSetOperation
 from pyrap.constants import d3v3, d3v4, d3v5
 from pyrap.events import OnSelect, OnSet, _rwt_event
-from pyrap.ptypes import BitField
-from pyrap.pwt.pwtutils import downloadsvg, downloadpdf
+from pyrap.ptypes import BitField, SVG
 from pyrap.widgets import Widget
 
+from svglib.svglib import svg2rlg
+from reportlab.graphics import renderPDF
+
 d3versions = {3: d3v3, 4: d3v4, 5: d3v5}
+
+
+def downloadsvg(cnt, w, h, css, name='tmpviz'):
+    svg = SVG()
+    svg.load(cnt, w=w, h=h, css=css)
+
+    stream = io.BytesIO()
+    svg.save(stream)
+    session.runtime.download('{}.svg'.format(name), 'image/svg+xml', stream.getvalue(), force=True)
+    stream.close()
+
+
+def downloadpdf(cnt, w, h, css, name='tmpviz'):
+    tmpfile = os.path.join(tempfile.gettempdir(), 'viz.svg')
+    svg = SVG()
+    svg.load(cnt, w=w, h=h, css=css)
+
+    with open(tmpfile, 'wb') as f:
+        svg.save(f)
+
+    svgcontent = svg2rlg(tmpfile)
+    v = renderPDF.drawToString(svgcontent)
+    session.runtime.download('{}.pdf'.format(name), 'application/pdf', v, force=True)
+    os.remove(tmpfile)
 
 
 class D3Widget(Widget):
